83cea23d29be7a64abd1bc31cb2fd608cec3bf5a
Marc Sluiter <marc@slintes.net>
2019-02-12 18:04:13 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"vanity2/pkg/test2"
+)
+
+func main() {
+
+	test2.Hello()
+
+}
---
+++ pkg/test2/test2.go
@@ -0,0 +1,7 @@
+package test2
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello from test2")
+}
